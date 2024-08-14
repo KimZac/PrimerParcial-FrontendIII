95d078d8c823395a2d782c7cb3e739ffc8ac5697
KimZac <kimzack28@gmail.com>
2024-08-13 19:55:07 -0600

    Primer parcil Front end III

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+//import { useState } from 'react'
+import './App.css'
+import Card from '../Components/card'
+
+function App() {
+ // const [count, setCount] = useState(0)
+
+  return (
+
+      <div className='App'>
+        <h1>Primer Parcial</h1>
+        <form></form>
+        <Card/>
+      </div>
+  )
+}
+
+export default App
